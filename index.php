4dabfe56c29a4744353c76930d37f0d4d71adba5
李国铃 <1277613171@qq.com>
2023-08-30 20:44:16 +0800

    测试

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+include __DIR__.'/vendor/autoload.php';
+var_dump(\Shenmuwangluo\YunZuoThinkcmf\Test::getValue());
---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Shenmuwangluo\YunZuoThinkcmf;
+
+class Test
+{
+   static public function getValue(){
+       return 'ok';
+   }
+}
